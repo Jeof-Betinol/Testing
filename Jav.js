db14f97bd4fe98f0942b76e643a274fdcc992000
Jeof-Betinol <jeofbetinol2007@gmail.com>
2026-05-14 13:13:13 +0800

    Update Jav.js

--- Jav.js
+++ Jav.js
@@ -27,31 +27,20 @@ const reserveNumber = document.getElementById("reserveNumber");
 
 //For sending email and showing reservation number
 signupForm.addEventListener("submit", function(event){
+
     event.preventDefault();
 
     const randomNumber = Math.floor(Math.random() * 100) + 1;
 
     reserveNumber.textContent = "No. " + randomNumber;
 
-    const templateParams = {
-        fname: document.getElementById("Fname").value,
-        lname: document.getElementById("Lname").value,
-        email: document.getElementById("Email").value,
-        date: document.getElementById("Rdate").value,
-        time: document.getElementById("Rtime").value,
-        number: randomNumber
-    };
-
-    emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", templateParams)
-    .then(function(response) {
-        console.log("Email sent!", response.status);
-    }, function(error) {
-        console.log("FAILED...", error);
-    });
-
     signupForm.style.display = "none";
+
     SuccessBox.style.display = "block";
+
 });
+
+
 
 SuccessBox.addEventListener("click", function(){
 
